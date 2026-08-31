b5d31e8953fc7862cfc6177d84e0eeb0faf48b84
Subharaj Nandi <subharajnandi2005@gmail.com>
2026-08-31 17:45:18 +0530

    Text Based Simple ToDo List

---
+++ 02_Simple ToDo list.py
@@ -0,0 +1,20 @@
+# Day 2 - 30th August 2026
+# Mini Project 2 - Text based simple To-Do list
+ToDo = []
+
+task1 = str(input("Enter Your task: "))
+ToDo.append(task1)
+
+task2 = str(input("Enter Your 2nd task: "))
+ToDo.append(task2)
+
+task3 = str(input("Enter Your 3rd task: "))
+ToDo.append(task3)
+
+task4 = str(input("Enter the 4th task: "))
+ToDo.append(task4)
+
+task5 = str(input("Enter the 5th task: "))
+ToDo.append(task5)
+
+print("\nYour To-Do List:", ToDo)
